bfe745a83ba8c586313189c753bd1defe197e5a1
jiro141 <felipe.colmenaresvzla@gmail.com>
2025-09-11 21:15:01 -0400

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useLocation } from "react-router-dom"; // 👈 Importar hook
+import { useLocation, Link } from "react-router-dom"; // 👈 Importamos Link
 import { BiLogOutCircle } from "react-icons/bi";
 import globo from "../img/globe_location_pin (1).svg";
 
 export default function Header({ toggleModal, userTipo }) {
-  const location = useLocation(); // 👈 Obtener ruta actual
+  const location = useLocation();
 
   const handleLogout = () => {
     localStorage.clear();
@@ -14,14 +14,17 @@ export default function Header({ toggleModal, userTipo }) {
   return (
     <div className="header">
       <div>
-        <img
-          src="https://detipcompany.com/wp-content/uploads/2024/10/cropped-PropuestaLogo.png"
-          alt="logo detip"
-          className="imagen"
-        />
+        {/* 👇 Usamos Link para navegar sin recarga */}
+        <Link to="/">
+          <img
+            src="https://detipcompany.com/wp-content/uploads/2024/10/cropped-PropuestaLogo.png"
+            alt="logo detip"
+            className="imagen"
+          />
+        </Link>
       </div>
       <div className="boton-pais">
-        {userTipo === 1 && location.pathname === "/" && ( // 👈 Mostrar solo en "/"
+        {userTipo === 1 && location.pathname === "/" && (
           <a onClick={toggleModal}>
             País
             <img src={globo} alt="Globo de ubicación" />
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -14,54 +14,42 @@ export default function Servicios({
   goToInformes
 }) {
   const cliente = formData?.cliente || {};
-  const isClienteIncompleto =
-    !cliente.nombre || !cliente.email || !cliente.cargo;
-  const isDisabled = isClienteIncompleto || userTipo !== 1;
 
-  const tooltipProps = isClienteIncompleto
-    ? {
-        "data-tooltip-id": "cliente-tooltip",
-        "data-tooltip-variant": "light",
-        "data-tooltip-content": "Por favor, cargar datos del cliente",
-      }
-    : {};
+
 
   return (
     <div
       className={`button-container-wrapper grid cuadrado `}
       style={{ flex: "1", minWidth: 0 }}
     >
-      <div {...tooltipProps}>
+      <div >
         <button
           className="button"
           onClick={goWebEsencial}
-          disabled={isClienteIncompleto}
+
         >
           <img src={webEsencial} alt="Web Esencial" />
           <p className="button-text">Web Esencial</p>
         </button>
       </div>
-      <div {...tooltipProps}>
-        <button className="button" onClick={goToAbout} disabled={isDisabled}>
+      <div >
+        <button className="button" onClick={goToAbout} >
           <img src={smart} alt="Smart Solutions" />
           <p className="button-text">Smart Solution</p>
         </button>
       </div>
-      <div {...tooltipProps}>
-        <button className="button" onClick={goToAbout} disabled={isDisabled}>
+      <div >
+        <button className="button" onClick={goToAbout} >
           <img src={digital} alt="Digital Support" />
           <p className="button-text">Support and Development</p>
         </button>
       </div>
-      <div {...tooltipProps}>
-        <button className="button" onClick={goToInformes} disabled={isDisabled}>
+      <div >
+        <button className="button" onClick={goToInformes} >
           <img src={informes} alt="Informes" />
           <p className="button-text">Informes</p>
         </button>
       </div>
-
-      {/* Renderiza solo un tooltip compartido */}
-      {isClienteIncompleto && <Tooltip id="cliente-tooltip" place="top" />}
     </div>
   );
 }
